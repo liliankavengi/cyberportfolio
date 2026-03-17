91e1e4fe962367c47fe55a96e65b3d9c024be6f6
liliankavengi <kavengililian14@gmail.com>
2026-03-17 11:50:18 +0300

    updated

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.after_request
-def add_security_headers(response):
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    response.headers['X-Frame-Options'] = 'DENY'
-    response.headers['X-XSS-Protection'] = '1; mode=block'
-    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-    response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self' https://kit.fontawesome.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com https://ka-f.fontawesome.com; img-src 'self' data:; connect-src 'self' https://ka-f.fontawesome.com;"
-    return response
-
 if __name__ == '__main__':
+
     app.run(debug=True, port=5000)
